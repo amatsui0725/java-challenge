339bec199d51ed698af452984b5bd20e05995bb4
Akira Matsui <akira.matsui86@hotmail.com>
2023-06-20 19:00:07 +0900

    implement cache

--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -3,12 +3,17 @@ package jp.co.axa.apidemo.services;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@CacheConfig(cacheNames = { "employees" })
 public class EmployeeServiceImpl implements EmployeeService {
 
     @Autowired
@@ -18,24 +23,29 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.employeeRepository = employeeRepository;
     }
 
+    @Cacheable
     public List<Employee> retrieveEmployees() {
         List<Employee> employees = employeeRepository.findAll();
         return employees;
     }
 
+    @Cacheable(key = "#employeeId")
     public Optional<Employee> getEmployee(Long employeeId) {
         Optional<Employee> optEmp = employeeRepository.findById(employeeId);
         return optEmp;
     }
 
+    @CacheEvict(allEntries = true)
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
 
+    @CacheEvict(key = "#employee.id")
     public void deleteEmployee(Long employeeId) {
         employeeRepository.deleteById(employeeId);
     }
 
+    @CachePut(key = "#employee.id")
     public Employee updateEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
